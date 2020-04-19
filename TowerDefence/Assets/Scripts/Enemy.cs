b85d414c922269b4d7143249f79a91deba651383
GilYoung04 <63942183+GilYoung04@users.noreply.github.com>
2020-04-19 13:38:10 +0900

    Test1
    asdaw

---
+++ TowerDefence/Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed;
+    Vector3 targetPos = Vector3.zero;
+    Tile currentTile = null;
+
+    private void Update()
+    {
+        if (targetPos != Vector3.zero)
+        {
+            transform.Translate((targetPos - transform.position)
+             .normalized * moveSpeed * Time.deltaTime);
+
+            if ((targetPos - transform.position).magnitude <= 0.3f)
+            {
+                transform.position = targetPos;
+                targetPos = Vector3.zero;
+                NextMove();
+            }
+        }
+    }
+
+    public void NextMove()
+    {
+        var targetTile = TileManager.Instance.GetNextWayPoint(currentTile);
+        if (targetTile == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Move(targetTile);
+    }
+
+    /// <summary>
+    /// 해당 타일로 이동하는 메소드
+    /// </summary>
+    /// <param name="tile">해당 타일</param>
+    public void Move(Tile tile)
+    {
+        currentTile = tile;
+        targetPos = tile.transform.position;
+
+          modelTrans.LookAt(
+            new Vector3(targetPos.x, modelTrans.position.y, targetPos.z)
+            );
+    }
+}
+    
+
+
+
---
+++ TowerDefence/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region 싱글톤
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_Instance == null)
+                _Instance = FindObjectOfType<GameManager>();
+
+            if (_Instance == null)
+            {
+                GameObject obj = new GameObject("Manager");
+                _Instance = obj.AddComponent<GameManager>();
+            }
+
+            return _Instance;
+        }
+    }
+    private static GameManager _Instance;
+    #endregion
+
+    public int leftEnemyCount;
+
+    public string[] enemyNames;
+
+    public void GameStart()
+    {
+        /// TileManager에서 맵 생성
+        TileManager.Instance.Build();
+        /// 적들을 생성 - N초마다 생성하는 기능
+        StartCoroutine(SpawnEnemy());
+    }
+
+    IEnumerator SpawnEnemy()
+    {
+        while (leftEnemyCount > 0)
+        {
+            yield return new WaitForSeconds(3f);
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        int randomIndex = Random.Range(0, enemyNames.Length);
+        string selectOne = enemyNames[randomIndex];
+        GameObject prefab = Resources.Load<GameObject>(selectOne);
+        GameObject obj = Instantiate(prefab);
+
+        obj.transform.position =
+            TileManager.Instance.spawnPoint.transform.position;
+    }
+
+    public void Gameover()
+    {
+        
+    }
+
+    [UnityEditor.MenuItem("게임메니저/게임 시작")]
+    public static void GameStart_EDITOR()
+    {
+        GameManager.Instance.GameStart();
+    }
+}
