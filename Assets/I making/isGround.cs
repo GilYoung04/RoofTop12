e069be0df6b3388bdba73b6baf0eeb225c2bf476
GilYoung04 <63942183+GilYoung04@users.noreply.github.com>
2020-04-26 17:28:35 +0900

    2Dm

---
+++ Assets/I making/isGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isGround : MonoBehaviour
+{
+    public movement targetScript;
+
+    // 유튜브를 보고 참고한 땅에 닿았는지 알려주는 센서 (중복 점프 방지를 위함)
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            targetScript.isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log(collision.collider.gameObject.name);
+        if (collision.collider.tag == "Ground")
+        {
+            targetScript.isGrounded = false;
+        }
+    }
+}
---
+++ Assets/I making/movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public bool  isGrounded = false;
+
+    // 얼마나 빠르게, 높이 할지 값을 설정할 수 있게 띄워논다.
+    public float moveSpeed = 2f;
+    public float jumpForce = 5f;
+
+    void Update()
+    {
+        // A,D / ← , →  를 눌렀을 때 좌우로 이동하게 해준다. 
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+
+        // 점프
+        if (Input.GetKeyDown(KeyCode.G) && isGrounded == true) // Trans: G키를 눌렀을때와 땅에 닿아있을때
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+            isGrounded = false;
+            Debug.Log("점프");
+        }
+    }
+    
+}
